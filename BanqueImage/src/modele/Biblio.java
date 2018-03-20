94e3f9d3d5e79c8c945e8b9eff6d0d3902127956
Unknown <raphael.talleux@gmail.com>
2018-03-20 11:49:53 +0100

    Bibliotheque
    Liste d'images recupérable a partir de Biblio.java

---
+++ BanqueImage/src/modele/Biblio.java
@@ -0,0 +1,19 @@
+package modele;
+
+import java.util.ArrayList;
+
+public class Biblio {
+	
+	public ArrayList<String> m_listeNom = new ArrayList();
+	
+	public Biblio()
+	{
+		for(int i=0; i<m_listeNom.size();i++)
+		{
+			m_listeNom.add(String.valueOf(i)+".jpg");
+		}
+	}
+	
+	 
+	
+}
