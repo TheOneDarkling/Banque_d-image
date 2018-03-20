8241fa318b11aa85e1fed5aa267baf33d418cac6
Unknown <raphael.talleux@gmail.com>
2018-03-20 10:35:06 +0100

    1er commit
    Ajout de la carte mentale / Ajout des collabo de github

---
+++ Banque images.mm
@@ -0,0 +1,98 @@
+<map version="freeplane 1.6.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Banque images" FOLDED="false" ID="ID_980603777" CREATED="1521534492056" MODIFIED="1521535251119" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" show_note_icons="true" show_icon_for_attributes="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="7" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Mod&#xe8;le" LOCALIZED_STYLE_REF="default" POSITION="right" ID="ID_1604776965" CREATED="1521534571491" MODIFIED="1521537126782" HGAP_QUANTITY="42.49999915063384 pt" VSHIFT_QUANTITY="-64.49999807775026 pt">
+<edge COLOR="#ff0000"/>
+<node TEXT="Classe biblio" ID="ID_803725844" CREATED="1521535401704" MODIFIED="1521537126781" HGAP_QUANTITY="52.2499988600612 pt" VSHIFT_QUANTITY="-0.7499999776482582 pt">
+<node TEXT="Matrice" ID="ID_1270967755" CREATED="1521536464848" MODIFIED="1521536717341" HGAP_QUANTITY="31.249999485909953 pt" VSHIFT_QUANTITY="-32.24999903887513 pt">
+<node TEXT="Recuperer data" ID="ID_1574783545" CREATED="1521536499344" MODIFIED="1521536714581" HGAP_QUANTITY="34.999999374151244 pt" VSHIFT_QUANTITY="-21.749999351799506 pt">
+<node TEXT="Taille" ID="ID_257265796" CREATED="1521536539536" MODIFIED="1521536714579" HGAP_QUANTITY="31.249999485909953 pt" VSHIFT_QUANTITY="-5.999999821186073 pt"/>
+<node TEXT="Nom" ID="ID_1851210505" CREATED="1521536591569" MODIFIED="1521536711796" HGAP_QUANTITY="27.49999959766866 pt" VSHIFT_QUANTITY="0.7499999776482591 pt"/>
+<node TEXT="TAGs" ID="ID_1034324938" CREATED="1521536598963" MODIFIED="1521536709354" HGAP_QUANTITY="25.999999642372142 pt" VSHIFT_QUANTITY="8.249999754130847 pt"/>
+</node>
+<node TEXT="Ajout data(apres ajout image)" ID="ID_477682722" CREATED="1521536569024" MODIFIED="1521536728802" HGAP_QUANTITY="40.99999919533732 pt" VSHIFT_QUANTITY="18.74999944120647 pt"/>
+</node>
+<node TEXT="Fonctions de recherche" ID="ID_1992489490" CREATED="1521536898147" MODIFIED="1521536911502" HGAP_QUANTITY="28.24999957531692 pt" VSHIFT_QUANTITY="17.249999485909953 pt"/>
+</node>
+</node>
+<node TEXT="Vue" POSITION="left" ID="ID_1103553673" CREATED="1521534754868" MODIFIED="1521537414620" HGAP_QUANTITY="56.74999872595075 pt" VSHIFT_QUANTITY="-6.74999979883433 pt">
+<edge COLOR="#0000ff"/>
+<node TEXT="Classe Fenetre" ID="ID_1420122873" CREATED="1521535141961" MODIFIED="1521537414619" HGAP_QUANTITY="39.4999992400408 pt" VSHIFT_QUANTITY="-35.99999892711642 pt">
+<node TEXT="Zone d&apos;affichage(miniatures)" ID="ID_52885656" CREATED="1521535800507" MODIFIED="1521535866236" HGAP_QUANTITY="43.2499991282821 pt" VSHIFT_QUANTITY="-47.24999859184031 pt"/>
+<node TEXT="Zone de recherche(TAGs)" ID="ID_1768684670" CREATED="1521535836987" MODIFIED="1521536204478" HGAP_QUANTITY="78.49999807775026 pt" VSHIFT_QUANTITY="0.7499999776482587 pt"/>
+<node TEXT="Selection TAILLE(liste)" ID="ID_227948223" CREATED="1521535853389" MODIFIED="1521536864180" HGAP_QUANTITY="46.24999903887513 pt" VSHIFT_QUANTITY="24.749999262392542 pt"/>
+</node>
+<node TEXT="Gestion des menus" ID="ID_1297574413" CREATED="1521537025156" MODIFIED="1521537156093" HGAP_QUANTITY="76.99999812245375 pt" VSHIFT_QUANTITY="-52.49999843537812 pt"/>
+<node TEXT="Affichage de la selection" ID="ID_535155168" CREATED="1521537158808" MODIFIED="1521537409668"/>
+</node>
+<node TEXT="Controlleur" POSITION="right" ID="ID_1149298044" CREATED="1521534762820" MODIFIED="1521537139046" HGAP_QUANTITY="35.74999935179952 pt" VSHIFT_QUANTITY="104.99999687075625 pt">
+<edge COLOR="#00ff00"/>
+<node TEXT="Recuperation d&apos;une liste depuis MODELE" ID="ID_1695983536" CREATED="1521536913555" MODIFIED="1521536997923"/>
+<node TEXT="Envoie de requettes au MODELE" ID="ID_78626732" CREATED="1521536940083" MODIFIED="1521536992936" HGAP_QUANTITY="14.749999977648258 pt" VSHIFT_QUANTITY="12.7499996200204 pt"/>
+<node TEXT="Gestion des" ID="ID_811127549" CREATED="1521537000339" MODIFIED="1521537139045" HGAP_QUANTITY="14.749999977648258 pt" VSHIFT_QUANTITY="7.499999776482589 pt"/>
+</node>
+</node>
+</map>
