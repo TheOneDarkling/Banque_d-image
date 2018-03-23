5c3e573a1a83c013f2d42cf5274d68257d40a5bf
Unknown <valentinpuech81@gmail.com>
2018-03-23 15:26:26 +0100

    afficheur 8 images

--- BanqueImage/src/vue/Afficher_images.java
+++ BanqueImage/src/vue/Afficher_images.java
@@ -5,6 +5,8 @@ import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -15,39 +17,55 @@ import javax.imageio.ImageIO;
 
 import modele.Biblio;
 
-public class Afficher_images extends Canvas implements Observer{
-	Biblio image= new Biblio();
-	Image[] img = new Image[image.m_listeNom.size()];
-	Image[] imgre = new Image[image.m_listeNom.size()];
+public class Afficher_images extends Canvas implements Observer,WindowListener{
+	Biblio image;
+	Image[] img ;
+	Image[] imgre ;
+	int posx=250;
+	int posy=50;
+	int hauteur =250;
+	int largeur=200;
+	int pas=50;
 	
 	
 	public Afficher_images(){
 		super();
+		
+		try {
+			this.image = new Biblio();
+		} catch (IOException e1) {
+			System.exit(-1);
+		}
+		this.img= new Image[image.m_listeImage.size()];
+		this.imgre= new Image[image.m_listeImage.size()];
 		Frame f = new Frame();
 		f.setSize(1500, 800);
 		f.setVisible(true);
 		f.add(this);
-		this.setPreferredSize(new Dimension(1500,800));
-		for(int i=0;i<image.m_listeNom.size();i++){
+		f.addWindowListener(this);
+		this.setPreferredSize(new Dimension(1400,700));
+		for(int i=0;i<image.m_listeImage.size();i++){
 			
 			try {
 				
-				 img[i]=ImageIO.read(new File(image.m_listeNom.get(i)));
+				 img[i]=ImageIO.read(new File(image.m_listeImage.get(i).m_lien));
 				 imgre[i]=img[i].getScaledInstance(250, 200,Image.SCALE_DEFAULT);
 			} catch (IOException e) {
-				throw new RuntimeException("L'image "+image.m_listeNom.get(i)+" est introuvable");
+				throw new RuntimeException("L'image "+image.m_listeImage.get(i)+" est introuvable");
 			}
 		}
-		repaint();
 		
+		repaint();
 
 		
 	}
 	
 	public void paint(Graphics g) {
-		for(int i=0;i<4;i++)
-		g.drawImage(imgre[i], 0+260*i, 0,250, 200, null);
-	}
+		for(int i=0;i<4;i++){
+		g.drawImage(imgre[i], 50+(posx+pas)*i, posy,hauteur, largeur, null);}
+		for(int i=0;i<4;i++ ){
+			g.drawImage(imgre[i+4], 50+(posx+pas)*i, posy+hauteur+pas,hauteur,largeur, null);}
+		}
 
 
 
@@ -57,9 +75,53 @@ public void update(Observable o, Object arg) {
 	
 }
 
+
+@Override
+public void windowActivated(WindowEvent arg0) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void windowClosed(WindowEvent arg0) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void windowClosing(WindowEvent arg0) {
+	System.exit(0);
+	
+}
+
+@Override
+public void windowDeactivated(WindowEvent arg0) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void windowDeiconified(WindowEvent arg0) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void windowIconified(WindowEvent arg0) {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public void windowOpened(WindowEvent arg0) {
+	// TODO Auto-generated method stub
+	
+}
+
 public static void main(String[] args) {
 	new Afficher_images();
 	
 	
 }
+
 }
