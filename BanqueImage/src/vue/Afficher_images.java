c4c02f0c7f57653779e0f0fd12a8813439e46feb
Unknown <valentinpuech81@gmail.com>
2018-03-23 14:50:30 +0100

    afficheur de 4 images,plus fenetre

--- BanqueImage/src/vue/Afficher_images.java
+++ BanqueImage/src/vue/Afficher_images.java
@@ -2,6 +2,8 @@ package vue;
 
 import java.awt.Canvas;
 import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -11,29 +13,53 @@ import java.util.Observer;
 
 import javax.imageio.ImageIO;
 
-public class Afficher_images extends Canvas implements Observer{{
-	Image[] img;
-	Image[] imgre;
-	public Afficher_images(Modele m){
-		super();
-		this.setPreferredSize(new Dimension(833,480));
-		
-		try {
-			 img[0]=ImageIO.read(new File(imageDefault));
-			 imgre[0]=img[0].getScaledInstance(, height, hints)
-		} catch (IOException e) {
-			throw new RuntimeException("L'image "+imageDefault+" est introuvable");
-		}
-		
-		m.addObserver(this);
+import modele.Biblio;
+
+public class Afficher_images extends Canvas implements Observer{
+	Biblio image= new Biblio();
+	Image[] img = new Image[image.m_listeNom.size()];
+	Image[] imgre = new Image[image.m_listeNom.size()];
 	
+	
+	public Afficher_images(){
+		super();
+		Frame f = new Frame();
+		f.setSize(1500, 800);
+		f.setVisible(true);
+		f.add(this);
+		this.setPreferredSize(new Dimension(1500,800));
+		for(int i=0;i<image.m_listeNom.size();i++){
+			
+			try {
+				
+				 img[i]=ImageIO.read(new File(image.m_listeNom.get(i)));
+				 imgre[i]=img[i].getScaledInstance(250, 200,Image.SCALE_DEFAULT);
+			} catch (IOException e) {
+				throw new RuntimeException("L'image "+image.m_listeNom.get(i)+" est introuvable");
+			}
+		}
+		repaint();
+		
+
 		
 	}
-}
+	
+	public void paint(Graphics g) {
+		for(int i=0;i<4;i++)
+		g.drawImage(imgre[i], 0+260*i, 0,250, 200, null);
+	}
+
+
 
 @Override
 public void update(Observable o, Object arg) {
 	// TODO Auto-generated method stub
+	
+}
+
+public static void main(String[] args) {
+	new Afficher_images();
+	
 	
 }
 }
