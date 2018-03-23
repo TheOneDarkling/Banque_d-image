406715127c0e984f23e9ae63cfde56339d645a89
Unknown <raphael.talleux@gmail.com>
2018-03-23 13:54:35 +0100

    Changement biblio + ajout listeFormats

--- BanqueImage/src/modele/Biblio.java
+++ BanqueImage/src/modele/Biblio.java
@@ -5,14 +5,19 @@ import java.util.ArrayList;
 public class Biblio {
 	
 	public ArrayList<String> m_listeNom = new ArrayList();
+	public ArrayList<String> m_listeFormat = new ArrayList();
+	int m_nbImages = 9;
 	
 	public Biblio()
 	{
-		for(int i=0; i<m_listeNom.size();i++)
+		for(int i=0; i<m_nbImages;i++)
 		{
 			m_listeNom.add("images/"+String.valueOf(i)+".jpg");
+			m_listeFormat.add("JPG");
 		}
 	}
+	
+	
 	
 	 
 	
