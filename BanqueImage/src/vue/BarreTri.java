4b42439ad8c22086860d5ab860b02f3870ee7b5e
Unknown <valentinpuech81@gmail.com>
2018-04-06 16:33:13 +0200

    barre de rechercher+barre de trie

--- BanqueImage/src/vue/BarreTri.java
+++ BanqueImage/src/vue/BarreTri.java
@@ -3,6 +3,7 @@ package vue;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Label;
 import java.awt.Panel;
 import java.awt.TextField;
@@ -22,15 +23,44 @@ public class BarreTri extends Panel {
 		String[] taille={"Taille superieur a","400 x 300","640 x 480","800 x 600","1024 x 768","1600 x 1200","2272 x 1704","2816 x 2112"};
 		String[] couleur={"Couleur dominante","Vert","Bleu","Rouge","Jaune","Marron","Noir","Blanc","Orange","Violet","Rose","Gris"};
 		String[] format={"Format ",".jpg",".bmp",".png",".gif",".tiff"};
+   
+        
 		JComboBox<String> Taille= new JComboBox<String>(taille);
 		JComboBox<String> Couleur= new JComboBox<String>(couleur);
 		JComboBox<String> Format= new JComboBox<String>(format);
-		TextField date =new TextField("    ");
+		
+		Font font = new Font("Arial",Font.BOLD,15);
+		Font fontbarre = new Font("Arial",Font.BOLD,13);
+		
+		TextField date =new TextField();
+		date.setColumns(10);
+		date.setFont(font);
+		
+		
 		Label lb = new Label("Saisir une annee :");
+		lb.setFont(font);
+		
+		
+		
+		Label lb2= new Label("Recherche par nom");
+		lb2.setFont(font);
+		
+		TextField nom = new TextField();
+		nom.setColumns(20);
+		nom.setFont(fontbarre);
+		
+		nom.setName("nom");
 		Taille.setName("Taille");
 		Couleur.setName("Couleur");
 		Format.setName("Format");
 		
+		
+		Taille.setFont(fontbarre);
+		Couleur.setFont(fontbarre);
+		Format.setFont(fontbarre);
+		
+		this.add(lb2);
+		this.add(nom);
 		this.add(Taille);
 		this.add(Couleur);
 		this.add(Format);
