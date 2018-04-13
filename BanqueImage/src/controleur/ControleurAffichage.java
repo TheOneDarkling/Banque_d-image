b49a221b2c11dd0d632eaeb107c866e1d3fc2ef2
Unknown <benji_ba@hotmail.fr>
2018-04-14 01:18:47 +0200

    Meilleure délimitation des images
    Maintenant, cliquer entre les images n'en agrandit aucune

--- BanqueImage/src/controleur/ControleurAffichage.java
+++ BanqueImage/src/controleur/ControleurAffichage.java
@@ -21,18 +21,35 @@ public ControleurAffichage(Biblio b){
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		Canvas zoneFleches = (Canvas)e.getSource();
+		int xCoord = e.getX();
+		int yCoord = e.getY();
+		
 		switch(zoneFleches.getName()) {
 			case "images":
-				int indexImage = e.getX()/(pasX+(largeur-(pasX*5))/4)+((e.getY()/((pasY+(pasX+(largeur-(pasX*5))/4))))*4);
-				indexImage = (indexImage>7)? indexImage-4: indexImage;
-				b.toggleImage();
-				b.setNumImage(indexImage + 8*modele.Constantes.numdebutdepage);
+				int imgDim = (largeur-(pasX*5))/4;
 				
+				int posX = xCoord/(pasX+imgDim);
+				posX = (posX==4)? 3 : posX;
+				
+				int posY = (yCoord/((pasY+(pasX+imgDim))));
+				posY = (posY > 1)? 1: posY;
+									
+				int imgBorderLeft = (posX+1)*pasX + posX*imgDim;
+				int imgBorderRight = (posX+1)*pasX+ (posX+1)*imgDim;
+				int imgBorderTop = posY*pasX+(posY+1)*pasY + posY*imgDim;
+				int imgBorderBottom = posY*pasX+(posY+1)*pasY+(posY+1)*imgDim;
+				
+				if(imgBorderLeft <= xCoord && xCoord <= imgBorderRight
+				&& imgBorderTop <= yCoord && yCoord <= imgBorderBottom) {
+					int indexImage = posX + 4*posY;
+					b.setNumImage(indexImage + 8*modele.Constantes.numdebutdepage);
+					b.toggleImage();
+				}else if(modele.Constantes.estengrand) {b.toggleImage();}
 				break;
 			case "fleches":
-				if(e.getX() > zoneFleches.getWidth()/2 && modele.Constantes.numdebutdepage < (b.m_listeImage.size()-1) / 8) {
+				if(xCoord > zoneFleches.getWidth()/2 && modele.Constantes.numdebutdepage < (b.m_listeImage.size()-1) / 8) {
 					b.setNumPage(modele.Constantes.numdebutdepage+1);
-				}else if(e.getX() <= zoneFleches.getWidth()/2 && modele.Constantes.numdebutdepage > 0){
+				}else if(xCoord <= zoneFleches.getWidth()/2 && modele.Constantes.numdebutdepage > 0){
 					b.setNumPage(modele.Constantes.numdebutdepage-1);
 				}
 				break;
